14a71e27b5c340286a3b6271ff0ae62f88b152c3
tauraspaj <pajuodis.tauras@gmail.com>
2021-03-31 15:37:49 +0100

    Display device list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,4 +9,29 @@ class UserController extends Controller
     public function index() {
         return User::all();
     }
+
+    public function deviceList() {
+        $devices = auth()->user()->devices();
+        return $devices;
+        
+        $result = array();
+        $i = 0;
+        foreach($devices as $device) {
+            $result[$i] = $device;
+            
+            if ($device->alarmTriggers()->find(1) != null) {
+                try {
+                    $result[$i]['isTriggered'] = $device->alarmTriggers()->where('isTriggered', 1)->get()[0]->isTriggered;
+                } catch(\Exception $e) {
+                    $result[$i]['isTriggered'] = 0;
+                }
+            } else {
+                $result[$i]['isTriggered'] = 0;
+            }
+            $i++;
+        }
+        return response()->json($result, 200);
+    }
 }
+
+
