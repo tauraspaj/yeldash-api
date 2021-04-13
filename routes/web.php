a4ddec1284516fde9671cdf10c0f1e300c9b0627
tauraspaj <pajuodis.tauras@gmail.com>
2021-04-13 13:18:49 +0100

    Updated routes

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,21 @@ $router->get('/', function () use ($router) {
     //root
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->post('user/auth', 'AuthController@login');
+$router->post('user/auth', 'AuthController@login');
 
-    $router->group(['middleware' => 'auth'], function () use ($router) {
-        $router->post('user/logout', 'AuthController@logout');
-        
-        $router->get('users', 'UserController@index');
-        $router->get('user/myProfile', 'UserController@myProfile');
-        $router->post('user/update', 'UserController@update');
-        $router->post('users', 'UserController@create');
-        // $router->delete('users/{userId}', 'UserController@index');
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->post('user/logout', 'AuthController@logout');
+    
+    $router->get('users', 'UserController@index');
+    $router->get('user/myProfile', 'UserController@myProfile');
+    $router->post('user/update', 'UserController@update');
+    $router->post('users', 'UserController@create');
+    // $router->delete('users/{userId}', 'UserController@index');
 
-        $router->get('devices/summary', 'DeviceController@deviceList');
-        
-        $router->group(['middleware' => 'deviceMiddleware'], function () use ($router) {
-            $router->get('devices/{deviceId}', 'DeviceController@show');
-            $router->put('devices/{deviceId}/customise', 'DeviceController@updateCustomise');
-        });
+    $router->get('devices/summary', 'DeviceController@deviceList');
+    
+    $router->group(['middleware' => 'deviceMiddleware'], function () use ($router) {
+        $router->get('devices/{deviceId}', 'DeviceController@show');
+        $router->put('devices/{deviceId}/customise', 'DeviceController@updateCustomise');
     });
 });
