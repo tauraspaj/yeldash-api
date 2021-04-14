f6d971559b055c8f42320754126fe78711ac228f
tauraspaj <pajuodis.tauras@gmail.com>
2021-04-14 13:44:58 +0100

    Change token ttl

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -66,9 +66,7 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'access_token' => $token
         ]);
     }
 }
